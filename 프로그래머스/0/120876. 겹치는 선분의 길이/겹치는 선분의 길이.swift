6bd2aeeab6b3e15465984f021f31ff958ff6bd5c
Seungjin Lee <105618997+SeungEEE@users.noreply.github.com>
2024-09-29 14:59:48 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 2.67 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+ func makelineArr(start: Int, end: Int)-> [Int] {
+     var result = [Int]()
+     for i in start...end {
+         result.append(i)
+     }
+     return result
+ }
+    
+func getOverlap(a: [Int], b: [Int]) -> [Int] {
+    var result = [Int]()
+    var isOn = false
+    let less = (a.count >= b.count) ? b : a
+    let more = (a.count >= b.count) ? a : b
+        
+    for i in 0..<less.count {
+        for j in 0..<more.count {
+            if less[i] == more[j] && (isOn == false) {
+                isOn = true
+            } else if less[i] == more[j] && (isOn == true) {
+                result.append(less[i])
+            }
+        }
+    }
+        
+    return result
+}
+    
+func solution(_ lines:[[Int]]) -> Int {
+    var result = [Int]()
+    let line1 = makelineArr(start: lines[0][0], end: lines[0][1])
+    let line2 = makelineArr(start: lines[1][0], end: lines[1][1])
+    let line3 = makelineArr(start: lines[2][0], end: lines[2][1])
+        
+    result += getOverlap(a: line1, b: line2)
+    result += getOverlap(a: line1, b: line3)
+    result += getOverlap(a: line2, b: line3)
+    result = Array(Set(result)).sorted()
+        
+    return result.count
+}
