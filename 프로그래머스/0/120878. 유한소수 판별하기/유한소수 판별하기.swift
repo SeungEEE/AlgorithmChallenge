89a56abcb608a3459a5dd54db65546ed063591de
Seungjin Lee <105618997+SeungEEE@users.noreply.github.com>
2024-09-30 23:14:18 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    var a = a
+    var b = b
+
+    while b % 2 == 0 { b = b / 2 }
+    while b % 5 == 0 { b = b / 5 }
+
+    return (a % b == 0) ? 1 : 2
+}
