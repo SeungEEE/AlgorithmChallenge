2670d89d6fccabec137aa57dd628e52afe9acf60
Seungjin Lee <105618997+SeungEEE@users.noreply.github.com>
2024-10-22 01:14:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 210.49 ms, Memory: 25.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ s:String) -> Int{
+    var stack = [Character]()
+    
+    for char in s {
+        if let last = stack.last, last == char {
+            stack.removeLast()
+        } else {
+            stack.append(char)
+        }
+    }
+    return stack.isEmpty ? 1 : 0
+}    
