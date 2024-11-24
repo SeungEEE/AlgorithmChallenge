06b29cdc32d087174ce531b2bffdaabd458ab043
Seungjin Lee <105618997+SeungEEE@users.noreply.github.com>
2024-11-24 23:47:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int64) -> Int64 {
+    let num = Int64(sqrt(Double(n)))
+    
+    if num * num == n {
+        return (num + 1) * (num + 1)  
+    } else {
+        return -1 
+    }
+}
